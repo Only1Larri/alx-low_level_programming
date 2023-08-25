ec3510cc5d54aa7faada813feab2b61cc095d36b
Martins Aguku <agukumartins@gmail.com>
2023-08-25 10:38:30 -0700

    Don't hate the hacker, hate the code

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -9,63 +9,49 @@
  */
 
 int main(void)
-
 {
-	char password[94];
-	int index = 0, sum = 0, diff_half1, diff_half2;
+    char password[94];
+    int index = 0, sum = 0, diff_half1, diff_half2;
 
-	srand(time(0));
+    srand(time(0));
 
-	while (sum < 2772)
+    while (sum < 2772)
+    {
+        password[index] = 33 + rand() % 94;
+        sum += password[index];
+        index++;
+    }
+    password[index] = '\0';
 
-	{
-		password[index] = 33 +  rand() % 94;
-		sum += password[index++];
-	}
+    if (sum != 2772)
+    {
+        diff_half1 = (2772 - sum) / 2;
+        diff_half2 = (2772 - sum) / 2;
 
-	password[index] = '\0';
+        if ((2772 - sum) % 2 != 0)
+        {
+            diff_half1++;
+        }
 
-	if (sum != 2772)
-	{
-		diff_half1 = (sum = 2772) / 2;
-		diff_half2 = (sum = 2772) / 2;
+        for (index = 0; password[index]; index++)
+        {
+            if (password[index] >= (33 + diff_half1))
+            {
+                password[index] -= diff_half1;
+                break;
+            }
+        }
 
-		if ((sum - 2772) % 2 != 0)
-						diff_half1++;
+        for (index = 0; password[index]; index++)
+        {
+            if (password[index] >= (33 + diff_half2))
+            {
+                password[index] -= diff_half2;
+                break;
+            }
+        }
+    }
 
-
-		for (index = 0; password[index]; index++)
-
-						{
-
-							if (password[index] >= (33 + diff_half1))
-
-							{
-				password[index] -= diff_half1;
-
-												break;
-
-			}
-
-						}
-
-						for (index = 0; password[index]; index++)
-
-						{
-
-							if (password[index] >= (33 + diff_half2))
-
-															{
-
-								password[index] -= diff_half2;
-
-								break;
-
-							}
-
-						}
-	}
-
-	printf("%s", password);
-	return (0);
+    printf("%s\n", password);
+    return (0);
 }
