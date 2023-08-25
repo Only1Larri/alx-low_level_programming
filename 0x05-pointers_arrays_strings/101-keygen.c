112ae2f4a262382fe4b6fa500fe0f0389a119407
Martins Aguku <agukumartins@gmail.com>
2023-08-25 11:42:42 -0700

    Don't hate the hacker, hate the code

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -11,7 +11,7 @@
 int main(void)
 {
 	int pass[100];
-	int i, sum, o;
+	int i, sum, n;
 
 	sum = 0;
 
@@ -22,7 +22,7 @@ int main(void)
 		pass[i] = rand() % 78;
 		sum += (pass[i] + '0');
 		putchar(pass[i] + '0');
-		if ((2772 - sum - '0' < 78)
+		if ((2772 - sum) - '0' < 78)
 		{
 			n = 2772 - sum - '0';
 			sum += n;
