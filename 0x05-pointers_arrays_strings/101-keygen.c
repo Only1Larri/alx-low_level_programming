0558fcf51a0bd24ee81f615221b7e2f11302f4f1
Martins Aguku <agukumartins@gmail.com>
2023-08-25 11:39:17 -0700

    Don't hate the hacker, hate the code

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -3,56 +3,33 @@
 #include <time.h>
 
 /**
- * main - program that generates random valid
- * passwords for the program 101-crackme
- * Return: Always 0 (Success)
+ * main - a program that generates random valid passwords
+ * for the program 101-crackme
+ * Return Always 0 (Success)
  */
 
 int main(void)
 {
-    char password[94];
-    int index = 0, sum = 0, diff_half1, diff_half2;
+	int pass[100];
+	int i, sum, o;
 
-    srand(time(0));
+	sum = 0;
 
-    while (sum < 2772)
-    {
-        password[index] = 33 + rand() % 94;
-        sum += password[index];
-        index++;
-    }
-    password[index] = '\0';
+	srand(time(NULL));
 
-    if (sum != 2772)
-    {
-        diff_half1 = (2772 - sum) / 2;
-        diff_half2 = (2772 - sum) / 2;
+	for (i = 0; i < 100; i++)
+	{
+		pass[i] = rand() % 78;
+		sum += (pass[i] + '0');
+		putchar(pass[i] + '0');
+		if ((2772 - sum - '0' < 78)
+		{
+			n = 2772 - sum - '0';
+			sum += n;
+			putchar(n + '0');
+			break;
+		}
+	}
 
-        if ((2772 - sum) % 2 != 0)
-        {
-            diff_half1++;
-        }
-
-        for (index = 0; password[index]; index++)
-        {
-            if (password[index] >= (33 + diff_half1))
-            {
-                password[index] -= diff_half1;
-                break;
-            }
-        }
-
-        for (index = 0; password[index]; index++)
-        {
-            if (password[index] >= (33 + diff_half2))
-            {
-                password[index] -= diff_half2;
-                break;
-            }
-        }
-    }
-
-    printf("Tada! Congrats\n");
-    fprintf(stderr, "%s\n", password);
-    return (0);
+	return (0);
 }
